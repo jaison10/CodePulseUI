6655cf06214b15f74085dd4a44a692d027401402
jaison10 <jaisonj1010@gmail.com>
2023-09-13 23:47:04 +0530

    unsubscribe on destroy

--- src/app/features/category/add-category/add-category.component.ts
+++ src/app/features/category/add-category/add-category.component.ts
@@ -1,36 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CreateCategory } from 'src/app/models/create-category-model';
 import { CategoryService } from '../category-service.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { Subscriber, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.css']
 })
-export class AddCategoryComponent implements OnInit {
+export class AddCategoryComponent implements OnInit,OnDestroy {
   category : CreateCategory = {
     'Name' : '',
     'UrlHandle' : ''
   }
-  
+  private createCategorySub ?: Subscription;
+
   constructor(
     private CategoryService : CategoryService, 
     private snackbar : MatSnackBar, 
     private router:Router
   ) { }
+  
 
   ngOnInit(): void {
   }
   public AddCategory(){
     console.log(this.category);
-    this.CategoryService.CreateCategory(this.category).subscribe((createdCat)=>{
+    this.createCategorySub = this.CategoryService.CreateCategory(this.category).subscribe((createdCat)=>{
       console.log("Created Category : ", createdCat);
       this.router.navigateByUrl('');
     }, (error)=>{
       console.log("Error occured: ", error);
     });
+  }
+  
+  ngOnDestroy(): void {
+    this.createCategorySub?.unsubscribe();
   }
 
 }
